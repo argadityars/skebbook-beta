125bfb4e53ed5c8853a1646119ee903b9bbabbbc
Argaditya RS <Argaditya RS>
2016-11-30 23:26:17 +0700

    Add user auth and some config

---
+++ config/secrets.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	// Mailer Configuration
+    'host' => 'mailtrap.io',
+    'username' => 'cde6da67a5b719',
+    'password' => 'ddcdbb8942b45d',
+    'port' => '465',
+    'encryption' => 'tls',
+];
